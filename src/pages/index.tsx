3ce697c7a579578a302bd9742d7ed2b0c147440f
Gustavo <gustavo_fior@outlook.com>
2024-08-07 08:25:40 -0300

    yolo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 
 import { api } from "~/utils/api";
 
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
@@ -24,6 +26,10 @@ export default function Home() {
   ]
 }'`;
 
+  const handleSignIn = (provider: string) => {
+    signIn(provider).catch((err) => console.error(err));
+  };
+
   const { data: sessionData } = useSession();
 
   return (
@@ -32,32 +38,41 @@ export default function Home() {
         <title>Iffy faster</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-900">
-        <div className="container flex max-w-2xl flex-col justify-center text-neutral-50">
-          <h1 className="text-2xl font-extrabold tracking-tight text-neutral-200">
-            Iffy but faster
-          </h1>
+      <main className="flex min-h-screen flex-col items-center bg-neutral-900 pt-12">
+        <div className="container flex max-w-3xl flex-col justify-center text-neutral-50">
+          <header className="flex items-center justify-between">
+            <h1 className="text-2xl font-semibold tracking-tighter text-neutral-200">
+              Iffy but faster
+            </h1>
+            <button
+              className="rounded-md bg-white/10 px-4 py-2 font-normal text-white text-sm transition hover:bg-white/20"
+              onClick={
+                sessionData
+                  ? () => void signOut()
+                  : () => handleSignIn("github")
+              }
+            >
+              {sessionData ? (
+                "Sign out"
+              ) : (
+                <div className="flex items-center gap-2">
+                  <p>Sign in with</p>
+                  <GitHubLogoIcon className="h-4 w-4" />
+                </div>
+              )}
+            </button>
+          </header>
+
           <div className="mt-4 rounded-xl bg-neutral-800 p-8 text-4xl text-neutral-200">
             <p className="text-sm text-neutral-400">
               Works just like iffy.com but faster:
               <br />
               <br />
-              <code className="mt-4 whitespace-pre-wrap text-sm text-neutral-200">
+              <code className="whitespace-pre-wrap text-sm text-neutral-200">
                 {curlCommand}
               </code>
             </p>
           </div>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <p className="text-center text-2xl text-white">
-            {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-          </p>
-          <button
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-            onClick={sessionData ? () => void signOut() : () => void signIn()}
-          >
-            {sessionData ? "Sign out" : "Sign in"}
-          </button>
         </div>
       </main>
     </>
